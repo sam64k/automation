741d259dea7fc8d60ab3b5c6156f2ea50efaacd7
sam64k <isamir.30@gmail.com>
2018-07-02 17:59:42 +0530

    adding files

---
+++ com.automation.maven/src/main/java/com/automation/maven/com/automation/maven/App.java
@@ -0,0 +1,13 @@
+package com.automation.maven.com.automation.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
