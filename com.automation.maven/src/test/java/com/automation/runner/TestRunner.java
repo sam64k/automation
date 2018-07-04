07d6ee94273225fb0da7925732843832f140b1ad
sam64k <isamir.30@gmail.com>
2018-07-04 18:15:41 +0530

    Added runner class

---
+++ com.automation.maven/src/test/java/com/automation/runner/TestRunner.java
@@ -0,0 +1,20 @@
+package com.automation.runner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+	features={"feature"},
+	glue={"com.automation.steps"},
+	tags={"@test1"},
+	//format={"pretty","html:target/Report"},
+	monochrome=true,
+	dryRun = false,
+	plugin = {"com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"}
+)
+public class TestRunner {
+
+}
