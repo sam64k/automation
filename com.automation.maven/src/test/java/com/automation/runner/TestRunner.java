43f1f8fef394f45e8b4288bd3e01b6df1c389a96
sam64k <isamir.30@gmail.com>
2018-07-05 18:02:08 +0530

    implemented extent report

--- com.automation.maven/src/test/java/com/automation/runner/TestRunner.java
+++ com.automation.maven/src/test/java/com/automation/runner/TestRunner.java
@@ -1,6 +1,11 @@
 package com.automation.runner;
 
+import java.io.File;
+
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
+
+import com.cucumber.listener.Reporter;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
@@ -12,9 +17,12 @@ import cucumber.api.junit.Cucumber;
 	tags={"@POC1"},
 	//format={"pretty","html:target/Report"},
 	monochrome=true,
-	dryRun = false
-	//plugin = {"com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"}
+	dryRun = false,
+	plugin = {"com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"}
 )
 public class TestRunner {
-
+	@AfterClass
+	public static void generateExtentReport(){
+	Reporter.loadXMLConfig(new File("C:\\Users\\IBM_ADMIN\\GIT-Maven\\com.automation.maven\\resources\\extent-report-config\\extent-config.xml"));
+	}
 }
