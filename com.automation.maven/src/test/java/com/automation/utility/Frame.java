8cf456eda8483251d18577184b0d57d2a3584621
sam64k <isamir.30@gmail.com>
2018-07-23 14:38:30 +0530

    added frame.class as utility

---
+++ com.automation.maven/src/test/java/com/automation/utility/Frame.java
@@ -0,0 +1,18 @@
+package com.automation.utility;
+
+public class Frame {
+	
+	public void switchByID(){
+		
+	}
+	public void switchByName(){
+		
+	}
+	public void switchByIndex(){
+		
+	}
+	public void switchToDefaultContent(){
+		
+	}
+
+}
